fa0329e2ba535297d30411195e018f6f212f9c3d
luizhenriquecervierirodrigues-cmyk <luizhenriquecervierirodrigues@gmail.com>
2026-03-28 21:21:49 -0400

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,31 @@
 <?php
 
-namespace App\Providers;
+    namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+    use Illuminate\Support\ServiceProvider;
+    use Illuminate\Support\Facades\URL; // <-- NÃO ESQUEÇA DESTA LINHA
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    class AppServiceProvider extends ServiceProvider
     {
-        //
-    }
+        /**
+         * Register any application services.
+         */
+        public function register(): void
+        {
+            //
+        }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+        /**
+         * Bootstrap any application services.
+         */
+        public function boot(): void
+        {
+            // Força HTTPS se não estiver em ambiente local
+            if (config('app.env') !== 'local') {
+                URL::forceRootUrl(config('app.url'));
+                URL::forceScheme('https');
+            }
+        }
     }
-}
+    
+?>
